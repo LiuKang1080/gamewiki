1fa748055bfd77b9ca1b3ade61d81b08b8ddee54
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-02-22 21:50:51 -0500

    Removed main.js and moved navbar and random_image function into their own .js files. Updated the js path link in nioh2.html. Updated equipment.html.

--- nioh2/js/background_image.js
+++ nioh2/js/background_image.js
@@ -1,51 +1,5 @@
 /*
 ========== ========== ==========
-JavaScript for Mobile Menu
-========== ========== ==========
-*/
-const initApp = () => {
-    /*
-    Function initializes the mobile version of the navbar.
-        - Get hamburger-button and mobile-menu elements.
-        - toggle the "hidden" attribute for mobile-menu.
-        - Add event listeners for the toggle.
-    
-    Parameters:
-    ----------
-    [none]
-
-    Return:
-    ----------
-    [none]
-    */
-    
-    // create variables:
-    const hamburger_button = document.getElementById("hamburger-button");
-    const mobile_menu = document.getElementById("mobile-menu");
-
-    // toggle the menu:
-    const toggle_menu = () => {
-        mobile_menu.classList.toggle('hidden');
-        mobile_menu.classList.toggle('flex');
-    }
-
-    // Add event listeners for clicks:
-    hamburger_button.addEventListener("click", toggle_menu);
-    mobile_menu.addEventListener("click", toggle_menu);
-}
-
-
-// call the initApp function
-/*
-    - listen to when the full HTML DOM has loaded, then call the initApp function
-    - If we try and call the function before the DOM has loaded in the buttons won't exist. Wait for the DOM to fully load, and
-        then call the initApp function.
-*/
-document.addEventListener("DOMContentLoaded", initApp);
-
-
-/*
-========== ========== ==========
 Randomly change background image upon page landing / refresh
 ========== ========== ==========
 */
