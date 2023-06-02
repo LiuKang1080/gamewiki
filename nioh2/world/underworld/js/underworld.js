b04bf77191c014234155e517ff6db1b5e54c17cc
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-06-02 02:53:47 -0400

    Updated comment in achievements.html. Added underworld .html and .js files.

---
+++ nioh2/world/underworld/js/underworld.js
@@ -0,0 +1,35 @@
+/* JavaScript for floors.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function floors() {
+    let element = document.getElementById("floors");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+let back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
