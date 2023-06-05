003b828a8d19a0183076a9a2c00e41767a8f5a42
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-06-04 22:55:02 -0400

    Added and completed depths .html and .js files. Added empty jizo.html. Updated the padding of the <h2> header in the Layers section in floors.html.

---
+++ nioh2/world/underworld/depths/js/depths.js
@@ -0,0 +1,30 @@
+/* JavaScript for depths.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
+
+
+/*
+========== ========== ========== ========== ==========
+Back to top button JavaScript 
+========== ========== ========== ========== ==========
+*/ 
+let back_to_top = document.getElementById("back-to-top-button");
+window.onscroll = function() { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+        back_to_top.style.display = "block";
+    } else {
+        back_to_top.style.display = "none";
+    }
+}
