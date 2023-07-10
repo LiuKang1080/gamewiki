7c258dfbe45368c5b550bf7e2d0e2da859509fc3
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-07-10 00:09:34 -0400

    Added picture_scrolls.html and the respective images in picture_scroll/img/.

---
+++ nioh2/equipment/picture_scrolls/js/picture_scrolls.js
@@ -0,0 +1,27 @@
+/* JavaScript for picture_scrolls.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function attributes_table() {
+    let element = document.getElementById("attributes_table");
+    element.scrollIntoView();
+}
+
+function ultimate_stats() {
+    let element = document.getElementById("ultimate_stats");
+    element.scrollIntoView();
+}
+
+function path_stats() {
+    let element = document.getElementById("path_stats");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
