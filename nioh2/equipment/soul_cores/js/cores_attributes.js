66abfdbfb8d4021ea4213fba9774d4c3d2220120
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-10-16 22:59:12 -0400

    Updated spacing in cores_list.html. Added the href link to Soul Cores Attribute Tables in cores_index.html. Added and completed cores_attributes .html and .js files.

---
+++ nioh2/equipment/soul_cores/js/cores_attributes.js
@@ -0,0 +1,62 @@
+/* JavaScript for cores_attributes.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function how_to() {
+    let element = document.getElementById("how-to");
+    element.scrollIntoView();
+}
+
+function anima_stats() {
+    let element = document.getElementById("anima-stats");
+    element.scrollIntoView();
+}
+
+function damage_stats() {
+    let element = document.getElementById("damage-stats");
+    element.scrollIntoView();
+}
+
+function ki_damage_stats() {
+    let element = document.getElementById("ki-damage-stats");
+    element.scrollIntoView();
+}
+
+function elemental_damage_stats() {
+    let element = document.getElementById("elemental-damage-stats");
+    element.scrollIntoView();
+}
+
+function idr_stats() {
+    let element = document.getElementById("idr-stats");
+    element.scrollIntoView();
+}
+
+function edr_stats() {
+    let element = document.getElementById("edr-stats");
+    element.scrollIntoView();
+}
+
+function luck_stats() {
+    let element = document.getElementById("luck-stats");
+    element.scrollIntoView();
+}
+
+function power_stats() {
+    let element = document.getElementById("power-stats");
+    element.scrollIntoView();
+}
+
+function no_conflict_stats() {
+    let element = document.getElementById("no-conflict-stats");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
