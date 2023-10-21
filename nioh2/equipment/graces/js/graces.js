c0c225443988da32f113b2fbb87bda3f9406181b
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-10-20 20:20:04 -0400

    Added the guardian spirit small fox logo for the Baku guardian spirit in guardian_spirits.html. Added graces .html and .js files. Added icon images for stats in picture_scrolls.html.

---
+++ nioh2/equipment/graces/js/graces.js
@@ -0,0 +1,50 @@
+/* JavaScript for graces.js */
+
+
+/* ========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ========== */
+function light() {
+    let element = document.getElementById("light");
+    element.scrollIntoView();
+}
+
+function medium() {
+    let element = document.getElementById("medium");
+    element.scrollIntoView();
+}
+
+function heavy() {
+    let element = document.getElementById("heavy");
+    element.scrollIntoView();
+}
+
+function demon() {
+    let element = document.getElementById("demon");
+    element.scrollIntoView();
+}
+
+function wise() {
+    let element = document.getElementById("wise");
+    element.scrollIntoView();
+}
+
+function nioh() {
+    let element = document.getElementById("nioh");
+    element.scrollIntoView();
+}
+
+function depths() {
+    let element = document.getElementById("depths");
+    element.scrollIntoView();
+}
+
+function description() {
+    let element = document.getElementById("description");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
