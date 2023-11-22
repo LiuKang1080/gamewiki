2e4098d380f753d47d950026a1ed1ba377950007
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-11-21 23:24:25 -0500

    Updated content in graces.html. Added hidden_skills .html and .js files. Added smithing_texts/img/sword_logo.png.

---
+++ nioh2/tools/smithing_texts/js/hidden_skills.js
@@ -0,0 +1,32 @@
+/* JavaScript for hidden_skills.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function weapon() {
+    let element = document.getElementById("weapon");
+    element.scrollIntoView();
+}
+
+function shiftling() {
+    let element = document.getElementById("shiftling");
+    element.scrollIntoView();
+}
+
+function ninjutsu() {
+    let element = document.getElementById("ninjutsu");
+    element.scrollIntoView();
+}
+
+function onmyo() {
+    let element = document.getElementById("onmyo");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
