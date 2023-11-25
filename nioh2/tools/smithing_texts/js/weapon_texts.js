19235fc1942a49200a50cf0ee1685210c16924ab
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-11-24 21:50:30 -0500

    Updating comments in smithing_texts_index.html. Updated content in hidden_skills.html. Added more images in smithing_texts/img/. Added weapon_texts .html and .js files.

---
+++ nioh2/tools/smithing_texts/js/weapon_texts.js
@@ -0,0 +1,75 @@
+/* JavaScript for weapon_texts.html */
+
+
+/* ========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ========== */
+function katana() {
+    let element = document.getElementById("katana");
+    element.scrollIntoView();
+}
+
+function dual() {
+    let element = document.getElementById("dual");
+    element.scrollIntoView();
+}
+
+function spear() {
+    let element = document.getElementById("spear");
+    element.scrollIntoView();
+}
+
+function axe() {
+    let element = document.getElementById("axe");
+    element.scrollIntoView();
+}
+
+function kusarigama() {
+    let element = document.getElementById("kusarigama");
+    element.scrollIntoView();
+}
+
+function odachi() {
+    let element = document.getElementById("odachi");
+    element.scrollIntoView();
+}
+
+function tonfa() {
+    let element = document.getElementById("tonfa");
+    element.scrollIntoView();
+}
+
+function hatchets() {
+    let element = document.getElementById("hatchets");
+    element.scrollIntoView();
+}
+
+function switchglaive() {
+    let element = document.getElementById("switchglaive");
+    element.scrollIntoView();
+}
+
+function splitstaff() {
+    let element = document.getElementById("splitstaff");
+    element.scrollIntoView();
+}
+
+function fists() {
+    let element = document.getElementById("fists");
+    element.scrollIntoView();
+}
+
+function bow() {
+    let element = document.getElementById("bow");
+    element.scrollIntoView();
+}
+
+function rifle() {
+    let element = document.getElementById("rifle");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
