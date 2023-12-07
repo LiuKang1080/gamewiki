5ec4d43bf42b14f637dac6ef53094984ba268ef5
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-12-06 21:02:26 -0500

    Updated content in weapon_texts.html. Added armor_texts .html and .js files. Added the respective images for head gear and armor_set .png files.

---
+++ nioh2/tools/smithing_texts/js/armor_texts.js
@@ -0,0 +1,20 @@
+/* JavaScript for armor_texts.html */
+
+
+/* ========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ========== */
+function headgear() {
+    let element = document.getElementById("headgear");
+    element.scrollIntoView();
+}
+
+function armor_set() {
+    let element = document.getElementById("armor-set");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
