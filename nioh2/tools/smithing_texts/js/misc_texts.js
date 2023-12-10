a4c8238043863f4c1cbb48bc2c047c8e38813c35
Shivakumar Mahakali <mahakalim@yahoo.com>
2023-12-10 01:03:57 -0500

    Added and completed misc_texts .html and .js files. Added misc_logo.png as the main header image for misc_texts.html.

---
+++ nioh2/tools/smithing_texts/js/misc_texts.js
@@ -0,0 +1,10 @@
+/* JavaScript for misc_texts.html */
+
+
+/* ========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ========== */
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
