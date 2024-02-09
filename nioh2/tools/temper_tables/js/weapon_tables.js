48df0f90d0e2b146170b70509184a9ed6707b053
Shivakumar Mahakali <mahakalim@yahoo.com>
2024-02-08 22:29:13 -0500

    Added more content to weapon_tables .js and .html files.

---
+++ nioh2/tools/temper_tables/js/weapon_tables.js
@@ -0,0 +1,17 @@
+/* JavaScript for weapon_tables.html */
+
+
+/* 
+========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ==========
+*/
+function nioh_gear() {
+    let element = document.getElementById("nioh-gear");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
