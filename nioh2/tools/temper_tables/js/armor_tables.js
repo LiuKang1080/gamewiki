e5d6486dda6f5d288add160f8c3f39d6ed454c9e
Shivakumar Mahakali <mahakalim@yahoo.com>
2024-02-23 20:04:50 -0500

    Added helmet_tables.html and armor_tables.js.

---
+++ nioh2/tools/temper_tables/js/armor_tables.js
@@ -0,0 +1,20 @@
+/* JavaScript for armor pages */
+
+
+/* ========== ========== ========== ========== ==========
+Table of Contents JavaScript 
+========== ========== ========== ========== ========== */
+function ethereal_gear() {
+    let element = document.getElementById("ethereal-gear");
+    element.scrollIntoView();
+}
+
+function nioh_gear() {
+    let element = document.getElementById("nioh-gear");
+    element.scrollIntoView();
+}
+
+function credits() {
+    let element = document.getElementById("credits");
+    element.scrollIntoView();
+}
