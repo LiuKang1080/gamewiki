e68e9e1650db3b42698ff81c6f4df405d8bca537
Shivakumar Mahakali <mahakalim@yahoo.com>
2026-02-06 17:12:36 -0500

    Added nioh3_index_image_4.png. Added nioh3_background_image.js. Added background_image.js function to nioh3.html.

---
+++ nioh3/js/nioh3_background_image.js
@@ -0,0 +1,38 @@
+/* ========== ========== ========== ========== ==========
+Randomly change background image upon page landing / refresh
+========== ========== ========== ========== ========== */
+function change_background_nioh3() {
+    /*
+    Function that randomly displayes the background image in the <body> tag of the page.
+        - Set up array of paths to the images (picture_array)
+        - Generate a random number from 0 to the length of picture_array
+        - Construct the final url as a string: "url( picture_array[random_number] )"
+        - Set the background-image using the complete_url [document.body.style.backgroundImage = value;]
+    
+    ==========
+    Parameters:
+        - [none]
+
+    ==========
+    Return:
+        - [none]
+    */
+
+    // Set up an array with images
+    let picture_array = [
+        './img/nioh2_index_image_4.png'
+    ];
+    
+    // get a random number
+    let random_number = Math.floor(Math.random() * picture_array.length);
+    
+    // construct the URL path for the background-image using the random number
+    let complete_url = String("url(" + picture_array[random_number] + ")");
+
+    // change the background image
+    document.body.style.backgroundImage = complete_url;
+}
+
+
+// call function to randomly change the background image
+change_background_nioh3();
